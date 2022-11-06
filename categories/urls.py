6c235d45994502857eda6b84e05e4e751553179a
soyeonnnb <la28s5d@naver.com>
2022-11-06 17:24:22 +0900

    feat: APIView 및 ModelSerializer로 변경

--- categories/urls.py
+++ categories/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.categories),  # /GET
-    path("<int:pk>", views.category),  # /GET
+    path("", views.Categories.as_view()),
+    path("<int:pk>", views.CategoryDetail.as_view()),  # /GET
 ]
