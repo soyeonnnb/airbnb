871f5d90a032c406e96ee49f20584d50000a71d6
soyeonnnb <la28s5d@naver.com>
2022-11-06 19:13:12 +0900

    feat: ModelViewSet으로 views.py 수정

--- categories/urls.py
+++ categories/urls.py
@@ -2,6 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Categories.as_view()),
-    path("<int:pk>", views.CategoryDetail.as_view()),  # /GET
+    path(
+        "",
+        views.CategoryViewSet.as_view(
+            {
+                # HTTP method와 class method를 연걸
+                "get": "list",  # 전체 queryset 리스트 가져옴
+                "post": "create",
+            }
+        ),
+    ),
+    path(
+        "<int:pk>",
+        views.CategoryViewSet.as_view(
+            {
+                "get": "retrieve",  # 여러개 중 하나만 가져옴
+                "put": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+    ),  # /GET
 ]
